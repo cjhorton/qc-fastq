213cdc6d60a57a637daa380a2b3529daef14a47d
cjhorton <jayhorton@gmail.com>
2025-06-19 16:07:22 -0400

    Wire uploader into the App component and add initial logic.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,32 @@
 import './App.css'
+import { FileUploader } from "@/components/file-uploader.tsx";
+import type { UploadedFiles } from "@/types/uploaded-files.ts";
+import { useState } from "react";
 
 export const App = () => {
+    const [files, setFiles] = useState<UploadedFiles>({read1: null, read2: null});
+
+    const handleAcceptedFiles = (read: '1' | '2', acceptedFiles: File[]) => {
+        setFiles(prev => ({
+            read1: read === '1' ? (acceptedFiles[0] ?? null) : prev.read1,
+            read2: read === '2' ? (acceptedFiles[0] ?? null) : prev.read2,
+        }));
+    };
+
+    const handleAnalyze = () => {
+        console.log('Analysis started!');
+    }
+
+    const isAnalyzeDisabled = !files.read1;
+
     return (
         <>
-            <h1>Coming Soon!</h1>
+            <FileUploader
+                showUpload={true}
+                onAcceptedFiles={handleAcceptedFiles}/>
+            <button onClick={handleAnalyze} disabled={isAnalyzeDisabled}>Analyze</button>
         </>
-    )
+    );
 }
 
 export default App
---
+++ src/types/uploaded-files.ts
@@ -0,0 +1,4 @@
+export type UploadedFiles = {
+    read1: File | null;
+    read2?: File | null;
+}
